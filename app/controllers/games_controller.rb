c9db231f3d758cb79d5694e36a7021aa1cef4f14
Diego Salgado <dasalgadob@unal.edu.co>
2021-08-09 08:57:22 -0500

    Add Game controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+class GamesController < ApplicationController
+
+  # POST /games
+  def create
+    @user_move = game_params
+    puts @user_move
+    @server_move = MoveCreator.new.get_move
+    unless @server_move
+      @server_move = %w(rock paper scissors).sample
+    end
+    puts @server_move
+    if @user_move == @server_move
+      @result = "tie"
+    elsif (@user_move == 'rock' &&  @server_move == 'scissors') || (@user_move == 'paper' &&  @server_move == 'rock') || (@user_move == 'scissors' &&  @server_move == 'rock')
+      @result = "win"
+    else
+      @result = "lose"
+    end
+    render json: {result: @result, server_move: @server_move}, status: :created
+  end
+
+  private
+    # Only allow a list of trusted parameters through.
+  def game_params
+    params.require(:user_move)
+  end
+end
